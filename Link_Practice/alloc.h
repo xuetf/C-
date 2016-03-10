b4862e88adb9f2092fbf9bbb07c14f0da856233d
薛涛锋 <24320132202505@stu.xmu.edu.cn>
2016-03-10 22:16:11 +0800

    Revert "insert"
    This reverts commit 83dc8c8a4cc0ae2cd7289bfa3a5164aef87963de.

--- Link_Practice/alloc.h
+++
@@ -1,5 +0,0 @@
-#include<cstdlib>
-#define malloc   
-#define MALLOC(num, type) (type *)alloc( (num) * sizeof(type) )
-
-extern void *alloc(size_t size);
--- Link_Practice/link_interface.h
+++ Link_Practice/link_interface.h
@@ -3,10 +3,5 @@ typedef struct NODE {
 	int value;
 }node;
 
-/**
-* 插入函数
-* 成功返回1,内存不够返回0.
-*/
-int insert(node ** linkp, int value);
 
 
